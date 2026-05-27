a8b9cdbea9859cf9c14788b49cf84c1d3c6461ca
BreakerChap <remyscalendar@gmail.com>
2026-05-27 14:13:41 +1000

    add navbar css and music page and route

--- app.py
+++ app.py
@@ -2,10 +2,14 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
 @app.route("/")
+@app.route("/home")
 def home():
-    return render_template("index.html")
+  return render_template("home.html")
+
+@app.route("/music")
+def music():
+  return render_template("music.html")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
